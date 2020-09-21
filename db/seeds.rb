715ddcf3367382b5d658035021595f89e8c36514
Vitor Santana <santana.vitor@gmail.com>
2020-09-21 19:23:30 -0300

    new migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,6 @@
 
 require 'open-uri'
 
-puts "Destroy ingredients"
-Ingredient.destroy_all if Rails.env.development?
-
-puts "Destroy Cocktails"
-Cocktail.destroy_all if Rails.env.development?
-
 puts "Create ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
@@ -21,5 +15,3 @@ ingredients["drinks"].each do |ingredient|
   i = Ingredient.create(name: ingredient["strIngredient1"])
   puts "create #{i.name}"
 end
-
-Cocktail.create(name: "Caipirinha")
